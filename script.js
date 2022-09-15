d349e64242b9818967cf4326ceeb7b278f89963a
nelson anguita <nelsonanguita@gmail.com>
2022-09-15 00:04:23 -0300

    carga inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+function getContenClick(event) {
+   const value = event.target.innerHTML
+   
+    filterAction(value)
+
+}
+
+
+const filterAction = value =>{
+    value = "9"
+}
+
+
+function addNumberInput(value) {
+
+    
+}
